7128d1baf16362e51f31f9aa4cc22367529062d0
Ricardo Lara <ricardo.lara2021@twk.cl>
2022-10-08 20:54:28 -0300

    Objeto creado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../models/login.model';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  id: any = '';
+  postNotes: Login[] = [];
+  modalTitle: string = 'Crear nota';
+  note: Login = {
+    rut: '',
+    password: ''
+  }
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/models/login.model.ts
@@ -0,0 +1,9 @@
+
+export class Login {
+    constructor(
+        public rut: string,
+        public password: string,
+        public _id?: any,
+
+    ){}
+}
