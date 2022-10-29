67e8329a06b503bb24a6df035707f15f17ca6ab6
Ricardo Lara <ricardo.lara2021@twk.cl>
2022-10-28 21:42:58 -0300

    objeto asignaturas creado, acordeon completo, pendiente back

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Course } from '../models/home.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  id: any = '';
+  user: Course = new Course('','','','','','','');
   constructor() { }
 
   ngOnInit(): void {
--- src/app/models/login.model.ts
+++ src/app/models/login.model.ts
@@ -1,4 +1,3 @@
-
 export class Teacher {
     constructor(
         public rut: string,
