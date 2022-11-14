5cf12d8bfba6b073d0fbe8195e9e58818535f696
Ricardo Lara <ricardo.lara2021@twk.cl>
2022-11-14 18:15:09 -0300

    Service asignatura

---
+++ src/app/services/subject.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Teacher } from '../models/login.model'
+import { Router } from '@angular/router'
+import { Course } from '../models/home.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectService {
+  
+  url = 'http://localhost:4000/subject/';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getSubjectByRut(user: Course): Observable<any> {
+    return this.http.post(this.url+'viewSubject', user);
+  }
+
+}
